e7698004196921626d3754cffad4e9b621b56cf5
Sayan3sarkar <sayansarkar333@gmail.com>
2020-07-21 19:27:05 +0530

    Adding a POST endpoint to add posts

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
 
 // CORS Handling Middleware
 app.use((req, res, next) => {
@@ -9,6 +13,14 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, X-Requested-With, Accept'); //List of headers of client to be allowed to access server
     next();
 });
+
+app.post('/api/posts', (req, res, next) => {
+    const post = req.body;
+    console.log(post);
+    res.status(201).json({
+        message: "Post Added Successfully"
+    })
+})
 
 app.use('/api/posts', (req, res, next) => {
     const posts = [{
