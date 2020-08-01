aa6f7fb900550fb19cf65d7599e10385b97571a9
Sayan3sarkar <sayansarkar333@gmail.com>
2020-08-01 19:36:05 +0530

    Outsourced auth.js logic to controllers/auth.js

---
+++ backend/controllers/auth.js
@@ -0,0 +1,74 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const User = require('../models/user');
+
+/**
+ * Controller for User Signup
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+exports.signupController = (req, res, next) => {
+    // const email = req.body.email;
+    const password = req.body.password;
+    bcrypt.hash(password, 12)
+        .then(hashedPw => {
+            const user = new User({
+                email: req.body.email,
+                password: hashedPw
+            });
+            user.save()
+                .then(result => {
+                    res.status(201).json({
+                        message: 'User Created',
+                        result: result
+                    });
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        error: err
+                    });
+                });
+        });
+};
+
+/**
+ * Controller for user Login
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+exports.loginController = (req, res, next) => {
+    let fetchedUser;
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({
+                    message: "Authentication Failed. Please Try again with valid credentials"
+                });
+            }
+            fetchedUser = user;
+            return bcrypt.compare(req.body.password, user.password);
+        })
+        .then(result => {
+            if (!result) {
+                return res.status(401).json({
+                    message: "Authentication Failed. Please Try again with valid credentials"
+                });
+            }
+            const token = jwt.sign({ email: fetchedUser.email, userId: fetchedUser._id },
+                `${process.env.JWT_SECRET_KEY}`, { expiresIn: '1h' }
+            );
+            res.status(200).json({
+                message: 'Logged In',
+                token: token
+            });
+        })
+        .catch(err => {
+            return res.status(401).json({
+                message: "Authentication Failed. Please Try again with valid credentials"
+            });
+        });
+};
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,68 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
 
-const User = require('../models/user');
+const authController = require('../controllers/auth');
 
 // Used for user signup. Endpoint: '<hostname>/api/auth/signup
-router.post('/signup', (req, res, next) => {
-    // const email = req.body.email;
-    const password = req.body.password;
-    bcrypt.hash(password, 12)
-        .then(hashedPw => {
-            const user = new User({
-                email: req.body.email,
-                password: hashedPw
-            });
-            user.save()
-                .then(result => {
-                    res.status(201).json({
-                        message: 'User Created',
-                        result: result
-                    });
-                })
-                .catch(err => {
-                    res.status(500).json({
-                        error: err
-                    });
-                });
-        });
-});
+router.post('/signup', authController.signupController);
 
 // Used for user login. Endpoint: '<hostname>/api/auth/login
-router.post('/login', (req, res, next) => {
-    let fetchedUser;
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({
-                    message: "Authentication Failed. Please Try again with valid credentials"
-                });
-            }
-            fetchedUser = user;
-            return bcrypt.compare(req.body.password, user.password);
-        })
-        .then(result => {
-            if (!result) {
-                return res.status(401).json({
-                    message: "Authentication Failed. Please Try again with valid credentials"
-                });
-            }
-            const token = jwt.sign({ email: fetchedUser.email, userId: fetchedUser._id },
-                `${process.env.JWT_SECRET_KEY}`, { expiresIn: '1h' }
-            );
-            res.status(200).json({
-                message: 'Logged In',
-                token: token
-            });
-        })
-        .catch(err => {
-            return res.status(401).json({
-                message: "Authentication Failed. Please Try again with valid credentials"
-            });
-        });
-});
+router.post('/login', authController.loginController);
 
 module.exports = router;
