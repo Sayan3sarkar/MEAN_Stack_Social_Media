6c72f4332b1fb8222f198ab20399773acedd43bb
Sayan3sarkar <sayansarkar333@gmail.com>
2020-08-01 14:25:47 +0530

    Added backend enpoint for user login using jwt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const User = require('../models/user');
 
+// Used for user signup. Endpoint: '<hostname>/api/auth/signup
 router.post('/signup', (req, res, next) => {
     // const email = req.body.email;
     const password = req.body.password;
@@ -25,6 +28,38 @@ router.post('/signup', (req, res, next) => {
                         error: err
                     });
                 });
+        });
+});
+
+// Used for user login. Endpoint: '<hostname>/api/auth/login
+router.post('/login', (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({
+                    message: "Authentication Failed. Please Try again with valid credentials"
+                });
+            }
+            return bcrypt.compare(req.body.password, user.password);
+        })
+        .then(result => {
+            if (!result) {
+                return res.status(401).json({
+                    message: "Authentication Failed. Please Try again with valid credentials"
+                });
+            }
+            const token = jwt.sign({ email: user.email, userId: user._id },
+                `${process.env.JWT_SECRET_KEY}`, { expiresIn: '1h' }
+            );
+            res.status(200).json({
+                message: 'Logged In',
+                token: token
+            });
+        })
+        .catch(err => {
+            return res.status(401).json({
+                message: "Authentication Failed. Please Try again with valid credentials"
+            });
         });
 });
 
