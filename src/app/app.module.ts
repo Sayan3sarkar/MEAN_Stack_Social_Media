69f3943a4b17eba4fc46aeb9f901224589d68990
Sayan3sarkar <sayansarkar333@gmail.com>
2020-08-03 10:53:41 +0530

    Added Angular-material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,8 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatPaginatorModule } from '@angular/material/paginator/';
-import { MatDialogModule } from '@angular/material/dialog';
-
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
@@ -22,6 +15,7 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor.service';
 import { ErrorInterceptor } from './error-interceptor.service';
 import { ErrorComponent } from './error/error.component';
+import { AngularMaterialModule } from '@modules/Angular-Material-Module/angular-material.module';
 
 @NgModule({
   declarations: [
@@ -39,14 +33,8 @@ import { ErrorComponent } from './error/error.component';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule,
     HttpClientModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
-    MatDialogModule
+    AngularMaterialModule
   ],
   providers: [
     {
